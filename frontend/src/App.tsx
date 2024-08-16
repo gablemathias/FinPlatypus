b0a884cddbc85f4966f67fdd359fbf63f408f108
Gabriel Mathias <gablemathias@gmail.com>
2024-08-16 16:21:01 -0300

    Searching event handler

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import CardList from './Components/CardList/CardList';
+import Search from './Components/Search/Search';
 
 
 function App() {
   return (
     <div className="App">
+      <Search />
       <CardList />
     </div>
   );
